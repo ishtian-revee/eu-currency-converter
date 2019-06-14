d5cfcb1c7f71aa3ac768f4873e4f72e4bf33431b
ishtian revee <ishtian11@gmail.com>
2019-06-14 17:35:29 +0600

    jitpacke added, all ui implemented, some minor changes

--- app/src/main/java/com/example/eucurrencyconverter/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/eucurrencyconverter/ui/activities/MainActivity.kt
@@ -2,12 +2,36 @@ package com.example.eucurrencyconverter.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.eucurrencyconverter.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // method calls
+        displayOriginalAmount()
+    }
+
+    // this method will set original amount text to whatever user input in the amount field
+    private fun displayOriginalAmount(){
+        et_currency_amount.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // set the text
+                originalAmount.text = s
+            }
+        })
     }
 }
