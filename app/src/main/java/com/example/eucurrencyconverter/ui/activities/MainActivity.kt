72572b764b2d5de20c339e33d8c3b5fed8459952
ishtian revee <ishtian11@gmail.com>
2019-06-15 23:15:26 +0600

    fetching rates data partially done, some extra codes for tests added

--- app/src/main/java/com/example/eucurrencyconverter/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/eucurrencyconverter/ui/activities/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.android.volley.*
 import com.android.volley.toolbox.JsonObjectRequest
@@ -16,7 +18,10 @@ import org.json.JSONObject
 class MainActivity : AppCompatActivity() {
 
     // Fields ==========================================================================================================
-    private var countries = ArrayList<String>()
+    private var mSelectedCountry = ""
+    private var mCountries = ArrayList<String>()
+    private var mVatRatesName = ArrayList<String>()
+    private var mVatRatesValue = ArrayList<String>()
     // =================================================================================================================
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,7 +36,7 @@ class MainActivity : AppCompatActivity() {
 
     // this method will fetch all the JSON data from the URL
     private fun getCountryNames(){
-        // creating request
+        // creating request for fetching country names data
         val request = object : JsonObjectRequest(Constants.API_METHOD_GET, Constants.API_URL, null,
             Response.Listener<JSONObject> { response ->
                 // get the root JSON array
@@ -42,7 +47,68 @@ class MainActivity : AppCompatActivity() {
                     val obj = jsonArray.getJSONObject(i)
                     // getting each country name and adding it to the list
                     val countryName = obj.getString("name")
-                    countries.add(countryName)
+                    mCountries.add(countryName)
+                }
+            },
+            Response.ErrorListener { error ->
+                error.printStackTrace()
+            }) {
+            @Throws(AuthFailureError::class)
+            override fun getHeaders(): Map<String, String> {
+                val headers = HashMap<String, String>()
+                headers.put("Content-Type", "application/json")
+                return headers
+            }
+        }
+        // add the request to the request queue
+        VolleySingleton.getInstance(this).addToRequestQueue(request)
+    }
+
+    // this method will fetch the vat rates data of selected country item for spinner
+    private fun getVatRatesData(){
+        // clearing rray lists
+        mVatRatesName.clear()
+        mVatRatesValue.clear()
+
+        // creating request for fetching vat rates
+        val request = object : JsonObjectRequest(Constants.API_METHOD_GET, Constants.API_URL, null,
+            Response.Listener<JSONObject> { response ->
+                // get the root JSON array
+                val rootArray = response.getJSONArray(Constants.API_ROOT_ENTRY)
+
+                // loop through each object
+                for(i in 0 until rootArray.length()){
+                    // takes each object
+                    val obj = rootArray.getJSONObject(i)
+
+                    // checking with the selected item name
+                    if(obj.get("name").equals(mSelectedCountry)){
+                        // get the periods JSON array
+                        val periods = obj.getJSONArray("periods")
+
+                        // loop through each periods object
+                        for(j in 0 until periods.length()){
+                            // get the each rates object
+                            val objRates = periods.getJSONObject(j)
+                            val rates = objRates.getJSONObject("rates")
+
+                            // get the keys
+                            val keys = rates.keys()
+                            // loop through all keys
+                            while (keys.hasNext()){
+                                // get each key names
+                                val key = keys.next().toString()
+                                mVatRatesName.add(key)
+                                // get each value
+                                val value = rates.getString(key)
+                                mVatRatesValue.add(value)
+                            }
+                            // test
+                            if(!mSelectedCountry.equals("Select")){
+                                test_1.text = mVatRatesName[2]
+                            }
+                        }
+                    }else{ break }
                 }
             },
             Response.ErrorListener { error ->
@@ -62,13 +128,30 @@ class MainActivity : AppCompatActivity() {
     // this method will initialize the spinner with country names
     private fun initSpinner(){
         // setting first entry as some text
-        countries.add("Select")
+        mCountries.add("Select")
         // create an Array Adapter using a simple sinner layout and languages array
-        val adapter = ArrayAdapter<String>(this, R.layout.spinner_item, countries)
+        val adapter = ArrayAdapter<String>(this, R.layout.spinner_item, mCountries)
         // set layout to use when the list of choices appear
         adapter.setDropDownViewResource(R.layout.spinner_drop_down_item)
         // set the adapter to spinner
         spinner_country.setAdapter(adapter)
+
+        // setting item click listener to the spinner
+        spinner_country.onItemSelectedListener = object: AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent:AdapterView<*>, view: View, position: Int, id: Long){
+                // get the selected item name
+                mSelectedCountry = parent.getItemAtPosition(position).toString()
+                // display the selected item text on test text view
+                test.text = mSelectedCountry
+
+                // method call
+                getVatRatesData()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>){
+                // Another interface callback
+            }
+        }
     }
 
     // this method will set original amount text to whatever user input in the amount field
